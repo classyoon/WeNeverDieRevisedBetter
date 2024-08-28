dbb59576faf132a53fead3dc8b6831749a3c5a82
classyoon <96508736+classyoon@users.noreply.github.com>
2024-08-27 21:52:50 -0400

    I set down the first layer, but swift data may be bounded to views which will seriously mess with our flow.

--- WeNeverDieRevisedBetter/Item.swift
+++ WeNeverDieRevisedBetter/Item.swift
@@ -16,3 +16,30 @@ final class Item {
         self.timestamp = timestamp
     }
 }
+
+class Game {
+    var saveSelection : [GameSaves]
+    var userSettings : UserSettings
+   
+    init(saveSelection: [GameSaves], userSettings: UserSettings) {
+        self.saveSelection = saveSelection
+        self.userSettings = userSettings
+    }
+}
+
+@Model
+class GameSaves {
+    var name : String
+    init(name: String = Date().description(with: .current)) {
+        self.name = name
+    }
+}
+
+@Model
+class UserSettings {
+    var leftHanded : Bool
+    init(leftHanded : Bool = false) {
+        self.leftHanded = leftHanded
+    }
+}
+
